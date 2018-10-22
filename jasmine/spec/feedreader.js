143c8a3fc580d850a48925a069c0787236c5c1f0
barbarastempien <barbara.stempien@gmail.com>
2018-10-22 22:33:37 +0200

    test:  Add test for feed's names
    For each feed, check if name is defined and not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -39,6 +39,12 @@ $(function() {
      * in the allFeeds object and ensures it has a name defined
      * and that the name is not empty.
      */
+    it('have defined and not empty names', () => {
+      for (const feed of allFeeds) {
+        expect(feed.name).toBeDefined();
+        expect(feed.name.length).not.toBe(0);
+      }
+    });
   });
 
 
